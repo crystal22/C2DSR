95988f188dcd905d6e65aafc9e49ecbd224d7768
DiMarzio Bian <326361233@qq.com>
2023-03-16 20:46:09 +0800

    updated

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def cal_metrics(ranks):
+def cal_metrics(ranks: list) -> list:
     """ calculate metrics hr, mrr, ndcg at k = 5, 20 """
     N = len(ranks)
     hr5, hr20, mrr5, mrr20, ndcg5, ndcg20 = 0., 0., 0., 0., 0., 0.
@@ -22,9 +22,10 @@ def cal_metrics(ranks):
 def cal_score(ranks_a, ranks_b, benchmark):
     """ calculate improvement rate and metrics for both domains """
     res = cal_metrics(ranks_a) + cal_metrics(ranks_b)
+    res_select = [res[0], res[4], res[6], res[10]]
 
-    imp = np.zeros(12)  # total 12 metrics
-    for i, (x, y) in enumerate(zip(res, benchmark)):
+    imp = np.zeros(len(benchmark))
+    for i, (x, y) in enumerate(zip(res_select, benchmark)):
         imp[i] = x / y - 1
 
     return [np.mean(imp)] + res
